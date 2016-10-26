e987ac5e1e29fa2e1c0dc6e850c05b7a3a780cd4
nickdibari <ndibari@fordham.edu>
2016-10-26 01:33:19 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,65 @@
+#! /usr/bin/python
+
+# Nicholas DiBari
+
+# Script to find email accounts in MailChimp account list
+# that are not in the OrgSync accounts list
+# Supports CSV Parsing for now
+
+# -IMPORTANT- #
+# Must pass the MailChimp file as first argument
+# Must pass Orgsync file as second argument
+
+# TODO: Extend file implementation
+# 	-> xlsx
+
+import csv
+
+import sys
+
+# PRE: File name of CSV file to parse
+# POST: Sorted list of Email Address attributes
+def CSVParser(to_parse):
+	results = []
+
+	with open(to_parse) as csvfile:
+		reader = csv.DictReader(csvfile)
+		for row in reader:
+			results.append(row['Email Address'])
+	results.sort()
+
+	return results
+
+# PRE: Two lists of Email Addresses to search
+# POST: One list of email addresses that are in MailChimp
+#		and not in OrgSync
+def GetTargets(Chimp, Org):
+	targets = []
+
+	for email in Chimp:
+		if email not in Org:
+			targets.append(email)
+	targets.sort()
+
+	return targets
+
+def main():
+	if len(sys.argv) != 3:
+		print('ERROR Usage ./main.py <MailChimp.csv> <OrgSync.csv>')
+		exit(1)
+
+	chimpFile = sys.argv[1]
+	orgFile = sys.argv[2]
+
+	MailChimp = CSVParser(chimpFile)
+	OrgSync = CSVParser(orgFile)
+
+	emails = GetTargets(MailChimp, OrgSync)
+
+	with open('emails.txt', 'w') as f:
+		# Write contents of target emails to text file
+		for email in emails:
+			f.write('{0}\n'.format(email))
+
+if __name__ == '__main__':
+	main()
